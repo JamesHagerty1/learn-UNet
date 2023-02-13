6edc981dcc701fdf0e4af2305cb14cf33e6b88b3
James <jhagerty@alumni.cmu.edu>
2023-02-13 17:43:18 -0500

    completely working training pipeline

--- train.py
+++ train.py
@@ -2,12 +2,12 @@ from dataset import SegmentationDataset
 from model import UNet
 from config import IMAGE_DATASET_PATH, MASK_DATASET_PATH, TEST_SPLIT, \
     TEST_PATHS, INPUT_IMAGE_HEIGHT, INPUT_IMAGE_WIDTH, BATCH_SIZE, PIN_MEMORY, \
-    DEVICE, INIT_LR, NUM_EPOCHS
+    DEVICE, INIT_LR, NUM_EPOCHS, PLOT_PATH, MODEL_PATH
 from torch.nn import BCEWithLogitsLoss
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
-from torchvision import transforms
+import torchvision # from torchvision import transforms
 from imutils import paths
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -35,9 +35,10 @@ def main():
     f.close()
 
     # torch datasets + dataloaders
-    transforms = transforms.Compose([transforms.ToPILImage(),
- 	    transforms.Resize((INPUT_IMAGE_HEIGHT, INPUT_IMAGE_WIDTH)),
-	    transforms.ToTensor()])
+    transforms = torchvision.transforms.Compose([
+        torchvision.transforms.ToPILImage(),
+ 	    torchvision.transforms.Resize((INPUT_IMAGE_HEIGHT, INPUT_IMAGE_WIDTH)),
+	    torchvision.transforms.ToTensor()])
     trainDS = SegmentationDataset(imagePaths=trainImages, maskPaths=trainMasks,
         transforms=transforms)
     testDS = SegmentationDataset(imagePaths=testImages, maskPaths=testMasks,
@@ -63,49 +64,61 @@ def main():
     startTime = time.time()
     for e in tqdm(range(NUM_EPOCHS)):
         unet.train()
-        
+
         # initialize the total training and validation loss
         totalTrainLoss = 0
         totalTestLoss = 0
         
         for (i, (x, y)) in enumerate(trainLoader):
-            # send the input to the device
-            (x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
-            # perform a forward pass and calculate the training loss
+            (x, y) = (x.to(DEVICE), y.to(DEVICE))
+           
             pred = unet(x)
             loss = lossFunc(pred, y)
-            # first, zero out any previously accumulated gradients, then
-            # perform backpropagation, and then update model parameters
+        
             opt.zero_grad()
             loss.backward()
             opt.step()
-            # add the loss to the total training loss so far
+
             totalTrainLoss += loss
-        # switch off autograd
+    
         with torch.no_grad():
-            # set the model in evaluation mode
             unet.eval()
             # loop over the validation set
             for (x, y) in testLoader:
-                # send the input to the device
-                (x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
+                (x, y) = (x.to(DEVICE), y.to(DEVICE))
                 # make the predictions and calculate the validation loss
                 pred = unet(x)
                 totalTestLoss += lossFunc(pred, y)
+
         # calculate the average training and validation loss
         avgTrainLoss = totalTrainLoss / trainSteps
         avgTestLoss = totalTestLoss / testSteps
+
         # update our training history
         H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
         H["test_loss"].append(avgTestLoss.cpu().detach().numpy())
         # print the model training and validation information
-        print("[INFO] EPOCH: {}/{}".format(e + 1, config.NUM_EPOCHS))
+        print("[INFO] EPOCH: {}/{}".format(e + 1, NUM_EPOCHS))
         print("Train loss: {:.6f}, Test loss: {:.4f}".format(
             avgTrainLoss, avgTestLoss))
-    # display the total time needed to perform the training
+    
     endTime = time.time()
     print("[INFO] total time taken to train the model: {:.2f}s".format(
         endTime - startTime))
+
+    # plot the training loss
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(H["train_loss"], label="train_loss")
+    plt.plot(H["test_loss"], label="test_loss")
+    plt.title("Training Loss on Dataset")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss")
+    plt.legend(loc="lower left")
+    plt.savefig(PLOT_PATH)
+
+    # serialize the model to disk
+    torch.save(unet, MODEL_PATH)
 
 if __name__ == "__main__":
     main()

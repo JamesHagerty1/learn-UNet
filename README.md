e6665e628740e1f4e3b7521afaae84100acc752b
James <jhagerty@alumni.cmu.edu>
2023-02-13 17:08:51 -0500

    data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://pyimagesearch.com/2021/11/08/u-net-training-image-segmentation-models-in-pytorch/
+
+Dataset:
+https://www.kaggle.com/competitions/tgs-salt-identification-challenge/data
+(only care about images and masks in "train" folder)
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from dataset import SegmentationDataset
 from model import UNet
-# from config import 
+from config import IMAGE_DATASET_PATH, MASK_DATASET_PATH, TEST_SPLIT, \
+    TEST_PATHS
 from torch.nn import BCEWithLogitsLoss
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -12,4 +13,28 @@ import matplotlib.pyplot as plt
 import torch
 import time
 import os
-print("hi")
+
+
+################################################################################
+
+
+def main():
+    imagePaths = sorted(list(paths.list_images(IMAGE_DATASET_PATH)))
+    maskPaths = sorted(list(paths.list_images(MASK_DATASET_PATH)))
+    
+    print(len(imagePaths), len(maskPaths))
+    return
+
+    split = train_test_split(imagePaths, maskPaths, test_size=TEST_SPLIT, 
+        random_state=42)
+    (trainImages, testImages) = split[:2]
+    (trainMasks, testMasks) = split[2:]
+    
+    print("[INFO] saving testing image paths...")
+    f = open(TEST_PATHS, "w")
+    f.write("\n".join(testImages))
+    f.close()
+
+
+if __name__ == "__main__":
+    main()

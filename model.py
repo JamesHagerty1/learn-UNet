bc51693587581d4414765f548cfbeb470911cb86
James <jhagerty@alumni.cmu.edu>
2023-02-14 17:45:47 -0500

    decoder ann

--- model.py
+++ model.py
@@ -26,16 +26,15 @@ class UNet(Module):
 		
 	# x (batch_size, channels, height, width)
 	def forward(self, x):
-		# encFeatures[i] (batch_size, channels, height, width)
+		# encFeatures[i] (batch_size, channels, height, width) 
+		# ascending values for channels
 		encFeatures = self.encoder(x)
+		# x from last Block of encoder pipeline, max channels val for dec or enc
+		x = encFeatures[-1]
+		decFeatures = self.decoder(x, encFeatures[::-1][1:])
+		
 
-
-		# pass the encoder features through decoder making sure that
-		# their dimensions are suited for concatenation
-		decFeatures = self.decoder(encFeatures[::-1][0],
-			encFeatures[::-1][1:])
-		# pass the decoder features through the regression head to
-		# obtain the segmentation mask
+		# regression head gives segmentation mask
 		map = self.head(decFeatures)
 		# check to see if we are retaining the original output
 		# dimensions and if so, then resize the output to match them
@@ -45,6 +44,7 @@ class UNet(Module):
 		return map
 
 
+# Storing convolution+pooling passes' results
 class Encoder(Module):
 	def __init__(self, channels=(3, 16, 32, 64)):
 		super().__init__()
@@ -62,7 +62,7 @@ class Encoder(Module):
 			# x (batch_size, channels', width', height')
 			x = block(x)
 			blockOutputs.append(x)
-			# x (batch_size, channels', width'//2, height'//2)
+			# x (batch_size, channels', width'//pool_dim, height'//pool_dim)
 			x = self.pool(x)
 		return blockOutputs
 
@@ -71,27 +71,36 @@ class Decoder(Module):
 	def __init__(self, channels=(64, 32, 16)):
 		super().__init__()
 		self.channels = channels
+		# Default [ConvTranspose2d(64, 32, ...), ConvTranspose2d(32, 16, ...)]
+		# self.upconvs[i] weight (in_channels, out_channels, 2, 2)
+		# self.upconvs[i] bias (out_channels)
 		self.upconvs = ModuleList(
 			[ConvTranspose2d(channels[i], channels[i + 1], 2, 2)
 			 	for i in range(len(channels) - 1)])
+		# Default [Block(64, 32), Block(32, 16)]
 		self.dec_blocks = ModuleList(
 			[Block(channels[i], channels[i + 1])
 			 	for i in range(len(channels) - 1)])
-		
+
+	# makes skip layer concatenations possible when there are slight 
+	# dim mismatches; matches stored encoder conv+pool result dims to dims of 
+	# upconvs to x 
 	def crop(self, encFeatures, x):
-		# grab the dimensions of the inputs, and crop the encoder
-		# features to match the dimensions
 		(_, _, H, W) = x.shape
 		encFeatures = CenterCrop([H, W])(encFeatures)
 		return encFeatures
 
+	# x (batch_size, channels, height, width)
 	def forward(self, x, encFeatures):
+		# upconv x (the final encoder conv+pool result) and concat its upconvs
+		# to the other encoder conv+pool results that skip-layer; respecting
+		# the U-shaped architecture (these concats increase channels),
+		# THEN pass x through conv+pool block
 		for i in range(len(self.channels) - 1):
+			# upconvs seem to undo BOTH encoder channel expansion and image
+			# height and width shrinking
+			# x (batch_size, channels', height', width')
 			x = self.upconvs[i](x)
-			# crop the current features from the encoder blocks,
-			# concatenate them with the current upsampled features,
-			# and pass the concatenated output through the current
-			# decoder block
 			encFeat = self.crop(encFeatures[i], x)
 			x = torch.cat([x, encFeat], dim=1)
 			x = self.dec_blocks[i](x)
